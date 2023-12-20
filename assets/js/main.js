f6f7789e09f872f7a51753b0b9c1d7edf1c0b501
bloraedufair <154106426+bloraedufair@users.noreply.github.com>
2023-12-20 15:34:15 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -327,4 +327,35 @@
       })
       .catch((error) => console.error("Error!", error.message));
   });
+
+  document.addEventListener("DOMContentLoaded", (event) => {
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    // Set the interval to 2000 milliseconds (2 seconds)
+    setInterval(function () {
+      plusDivs(1);
+    }, 17000);
+
+    function plusDivs(n) {
+      showDivs((slideIndex += n));
+    }
+
+    window.plusDivs = plusDivs; // This makes the function accessible globally
+
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      if (n > x.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = x.length;
+      }
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      x[slideIndex - 1].style.display = "block";
+    }
+  });
 })();
